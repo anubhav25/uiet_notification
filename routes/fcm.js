2fe4bed5b713e44abe4326a8e34c5abadf0b79c0
ANUBHAV <gupta.anubhav25@gmail.com>
2017-12-23 18:02:56 +0530

    fcm key fixes

--- routes/fcm.js
+++ routes/fcm.js
@@ -1,11 +1,10 @@
-module.exports =  function(devices,title,body,mydata){
 
 
 var admin = require("firebase-admin");
  // var serverKey = process.env.noti
-var serviceAccount = require('../noti.json')
-console.log(serviceAccount)
-//var serviceAccount = process.env.noti;
+//var serviceAccount = require('../noti.json')
+
+var serviceAccount = process.env.noti;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -16,6 +15,10 @@ var topic = "/topics/uietnews";
 console.log(topic);
 // See the "Defining the message payload" section below for details
 // on how to define a message payload.
+
+module.exports =  function(devices,title,body,mydata){
+
+
 var payload = {
     notification: {
             title: title,
